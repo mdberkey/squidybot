488d3a456d4216b93400ba362ce1e2222bf1bd74
mdberkey <mberkey2@gmail.com>
2024-02-28 23:15:20 -0500

    add board progress

--- src/board/board.go
+++ src/board/board.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+type Bitboard uint64
+type Piece uint
+type Side uint
+type Square uint
+
+const (
+	WHITE Side = 0
+	BLACK Side = 1
+	BOTH Side = 2
+	NUM_OF_PIECE_TYPES uint = 6
+	NUM_OF_CASTLING_PERMISSIONS uint = 16
+	NUM_OF_SQUARES uint = 64
+	NUM_OF_FILES uint = 8
+	NUM_OF_RANKS uint = 8
+	CASTLING_WK uint8 = 1
+	CASTLING_WQ uint8 = 2
+	CASTLING_BK uint8 = 4
+	CASTLING_BQ uint8 = 8
+	CASTLING_ALL uint8 = 15
+	EMPTY Bitboard = 0
+	MAX_GAME_MOVES uint = 2048
+	MAX_LEGAL_MOVES uint = 255
+	MAX_PLY int8 = 125
+	MAX_MOVE_RULE uint8 = 100
+	FEN_START_POSITION string = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+)  
+
+type board struct {
+	bb_pieces [BOTH][NUM_OF_PIECE_TYPES]Bitboard
+	bb_sides [BOTH]Bitboard
+	game_state int // TODO: implement
+	history int // TODO: implement
+	piece_list [NUM_OF_SQUARES]Piece
+}
+
+func newBoard() {
+	temp_pieces := board{}.bb_pieces
+}
 
 func printBitboard(bitboard uint64) {
 	const lastBit uint64 = 63
@@ -19,5 +59,6 @@ func printBitboard(bitboard uint64) {
 }
 
 func main() {
-	printBitboard()
+	printBitboard(123456)
+	newBoard()
 }  
